738a20cbc88525a97fb94ff578e7d78cc61e2b4f
Renan Andres Garcia <andresgarcia17@icloud.com>
2021-03-15 22:06:28 -0500

    changes

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,49 +1,51 @@
 export function getStrapiURL(path) {
   return `${
-    process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337'
-  }${path}`
+    process.env.NEXT_PUBLIC_STRAPI_API_URL ||
+    "https://back-pwa-med.herokuapp.com"
+  }${path}`;
 }
 
+//http://localhost:1337
 // Helper to make GET requests to Strapi
 
 export async function fetchAPI(path, options = {}) {
   const defaultOptions = {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-  }
+  };
   const mergedOptions = {
     ...defaultOptions,
     ...options,
-  }
-  const requestUrl = getStrapiURL(path)
-  const response = await fetch(requestUrl, mergedOptions)
+  };
+  const requestUrl = getStrapiURL(path);
+  const response = await fetch(requestUrl, mergedOptions);
 
   if (!response.ok) {
-    console.error(response.statusText)
-    throw new Error(`An error occured please try again`)
+    console.error(response.statusText);
+    throw new Error(`An error occured please try again`);
   }
-  const data = await response.json()
-  return data
+  const data = await response.json();
+  return data;
 }
 
 export async function getPageData(slug, preview = false) {
   // Find the pages that match this slug
   const pagesData = await fetchAPI(
-    `/pages?slug=${slug}&status=published${preview ? '&status=draft' : ''}`
-  )
+    `/pages?slug=${slug}&status=published${preview ? "&status=draft" : ""}`
+  );
 
   // Make sure we found something, otherwise return null
   if (pagesData == null || pagesData.length === 0) {
-    return null
+    return null;
   }
 
   // Return the first item since there should only be one result per slug
-  return pagesData[0]
+  return pagesData[0];
 }
 
 // Get site data from Strapi (metadata, navbar, footer...)
 export async function getGlobalData() {
-  const global = await fetchAPI('/global')
-  return global
+  const global = await fetchAPI("/global");
+  return global;
 }
